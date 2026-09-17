653bae6bc96d3b6053a0cd76c1e6691e77a356ae
subashaadhi <subashaadhi950@gmail.com>
2026-09-17 13:05:13 +0530

    add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import PaymentSuccessPage from './components/SpecifiedComponents/Home/Payment/PaymentSuccessPage'
 import POS from './Pages/POS'
-import PMLayout from './components/WrapperComponents/PMLayout'
-import PM from './Pages/PM'
-import WH from './Pages/WH'
-
+import PayPage from './Pages/PayPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  
-
   return (
     <>
-    {/* <POS/> */}
-    <PM />
-    {/* <WH /> */}
-    
- 
+      <Router>
+        <Routes>
+          <Route path="/" element={<POS />} />
+          <Route path="/payment" element={<PayPage />} />
+          <Route
+            path="/payment-success"
+            element={<PaymentSuccessPage />}
+          />
+        </Routes>
+      </Router>
     </>
   )
 }
